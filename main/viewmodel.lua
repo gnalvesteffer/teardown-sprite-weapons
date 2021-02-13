33932bf828fe6d4d021e55006e1781fdf2842f80
ss-gnalvesteffer <gavin.alvesteffer@shipstation.com>
2021-02-13 02:01:10 -0600

    add breathing sway

--- main/viewmodel.lua
+++ main/viewmodel.lua
@@ -20,13 +20,21 @@ local function get_current_weapon_frame_image_path()
     return weapon_state_definition.frames[frame_number]
 end
 
-local function draw_weapon()
+local function get_weapon_sway_amount()
     local player_speed = VecLength(GetPlayerVelocity())
     local movement_amount = math.clamp(player_speed * math.clamp(state.movement_time, 0, 1), 0, 10)
+    local breathing_amount = 3 -- todo: tie this to some sort of stamina variable?
+    return {
+        x = (breathing_amount + math.sin(GetTime() * 1.3) * breathing_amount) + (movement_amount + math.sin(state.movement_time * 10) * movement_amount),
+        y = (breathing_amount + math.cos(GetTime() * 2) * breathing_amount) + (movement_amount + math.sin(state.movement_time * 20) * movement_amount)
+    }
+end
 
+local function draw_weapon()
+    local weapon_sway_amount = get_weapon_sway_amount()
     UiPush()
     UiAlign("top left")
-    UiTranslate(movement_amount + math.sin(state.movement_time * 10) * movement_amount, movement_amount + math.sin(state.movement_time * 20) * movement_amount)
+    UiTranslate(weapon_sway_amount.x, weapon_sway_amount.y)
     UiImage(get_current_weapon_frame_image_path())
     UiPop()
 end
