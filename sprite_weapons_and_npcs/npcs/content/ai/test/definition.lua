2c5a5afe032ddbde6346cf35d92d331cafb4523a
ss-gnalvesteffer <gavin.alvesteffer@shipstation.com>
2021-02-15 17:16:34 -0600

    improve ai vision

--- sprite_weapons_and_npcs/npcs/content/ai/test/definition.lua
+++ sprite_weapons_and_npcs/npcs/content/ai/test/definition.lua
@@ -1,8 +1,8 @@
 local function can_npc_see_player(npc)
     local npc_height = npc:get_current_state_definition().npc_height
     local npc_eye_position = VecAdd(npc.position, Vec(0, npc_height, 0)) -- assume npc's eyes are at the top of the sprite
-    local player_eye_position = GetCameraTransform().pos
-    local is_line_of_sight_to_player_occluded = QueryRaycast(npc_eye_position, VecNormalize(VecSub(player_eye_position, npc_eye_position)), 100)
+    local top_of_player_head_position = VecAdd(GetCameraTransform().pos, Vec(0, 0.55, 0))
+    local is_line_of_sight_to_player_occluded = QueryRaycast(npc_eye_position, VecNormalize(VecSub(top_of_player_head_position, npc_eye_position)), 100)
     return not is_line_of_sight_to_player_occluded
 end
 
